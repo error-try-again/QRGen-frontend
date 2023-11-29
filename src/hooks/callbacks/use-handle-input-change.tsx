73c36f42b110ebb862418d355b1c530d62139104
void <yane.clarke@gmail.com>
2023-11-29 15:18:58 +1000

    - Made useHandleInputChange more generic

--- src/hooks/callbacks/use-handle-input-change.tsx
+++ src/hooks/callbacks/use-handle-input-change.tsx
@@ -1,4 +1,4 @@
-import { useCallback } from 'react';
+import { ChangeEvent, useCallback } from 'react';
 import { QRCodeRequest } from '../../ts/interfaces/qr-code-request-interfaces';
 import { useCore } from '../use-core';
 
@@ -6,7 +6,21 @@ export const useHandleInputChange = () => {
   const { dispatch, state } = useCore();
 
   return useCallback(
-    (value: string, fieldName: keyof QRCodeRequest) => {
+    (
+      valueOrEvent:
+        | string
+        | ChangeEvent<HTMLInputElement>
+        | ChangeEvent<HTMLTextAreaElement>,
+      fieldName: keyof QRCodeRequest
+    ) => {
+      let value: string;
+
+      if (typeof valueOrEvent === 'string') {
+        value = valueOrEvent;
+      } else {
+        value = valueOrEvent.target.value;
+      }
+
       if (state[fieldName] !== value) {
         dispatch({
           type: 'SET_FIELD',
